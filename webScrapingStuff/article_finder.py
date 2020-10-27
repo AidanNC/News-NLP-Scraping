93c9976cf8fc2185ec0ee72908066a41a9cd41fd
Sharqosity <typingpalguy@gmail.com>
2020-10-27 16:55:50 -0400

    Added parsing for nytimes

--- webScrapingStuff/article_finder.py
+++ webScrapingStuff/article_finder.py
@@ -3,27 +3,36 @@ from bs4 import BeautifulSoup
 
 
 url = 'https://www.foxnews.com/politics/supreme-court-strikes-down-state-ban-on-taxpayer-funding-for-religious-schools'
+urlNYT = 'https://www.nytimes.com/2020/10/26/us/politics/trump-barrett.html'
 
-def getArticle(url,site):
+
+def getArticle(url, site):
     page = requests.get(url)
     contents = page.content
     soup = BeautifulSoup(contents, 'html.parser')
     if(site == 'fox'):
-        paragraphs = soup.find('div',{"class": 'article-body'}).findAll('p')
-        bad_paragraphs = soup.find('div',{"class": 'article-body'}).findAll(href=True)
+        paragraphs = soup.find('div', {"class": 'article-body'}).findAll('p')
+        bad_paragraphs = soup.find('div', {"class": 'article-body'}).findAll(href=True)
 
-        #we want to get rid of the annoying links in the middle of the articles
-        for i in range(0,len(bad_paragraphs)):
+        # we want to get rid of the annoying links in the middle of the articles
+        for i in range(0, len(bad_paragraphs)):
             bad_paragraphs[i] = bad_paragraphs[i].getText()
         returner = ""
         for paragraph in paragraphs:
             temp = paragraph.getText()
             if temp not in bad_paragraphs:
-                returner += temp + " " 
+                returner += temp + " "
         return returner
-        
-    #paragraphs = soup.findAll("div",{"class": html_class})
-    #this is the basic format of returning the paragarphs
+
+    if (site == 'nyt'):
+        paragraphs = soup.find('section', {"name": 'articleBody'}).findAll('p')
+        returner = ""
+        for paragraph in paragraphs:
+            temp = paragraph.getText()
+            returner += temp + " " 
+        return returner
+    # paragraphs = soup.findAll("div",{"class": html_class})
+    # this is the basic format of returning the paragarphs
     '''
     returner = ""
     for val in paragraphs:
@@ -31,5 +40,6 @@ def getArticle(url,site):
     return returner
     '''
 
-print(getArticle(url,'fox'))
-#getArticle(url,'fox')
+#print(getArticle(url,'fox'))
+print(getArticle(urlNYT, 'nyt'))
+# getArticle(url,'fox')
