61c7244320713f8f86f83b0a7a347a56baf63cc3
Aidan <aidan.bnc@gmail.com>
2020-10-27 18:18:51 -0400

    Same as last commit.

--- webScrapingStuff/url_finder.py
+++ webScrapingStuff/url_finder.py
@@ -1,17 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 #this gets the html content of whatever webpage you want
 page = requests.get('https://www.foxnews.com/politics/mcconnell-urges-white-house-not-to-make-coronavirus-deal-with-pelosi')
 contents = page.content
 
 all_urls = []
+bad_urls = []
 
-stem = 'https://www.foxnews.com/politics'
-url = 'https://www.foxnews.com/politics/mcconnell-urges-white-house-not-to-make-coronavirus-deal-with-pelosi'
+regex = "/article/us"
+url = 'https://www.reuters.com/article/us-usa-election-biden-unions/two-largest-u-s-airline-flight-attendant-unions-endorse-biden-idUSKBN27C2VY'
 depth = 0
-def link_stem_finder(url, stem, depth):
-    if depth > 25 or len(all_urls) > 1000:
+def link_stem_finder(url, regex, depth, add_link = True):
+    #make sure that this url is accounted for
+    if add_link:
+        all_urls.append(url)
+    print(len(all_urls))
+    if depth > 2 or len(all_urls) > 1000:
         return
     page = requests.get(url)
     contents = page.content
@@ -19,18 +25,51 @@ def link_stem_finder(url, stem, depth):
     soup = BeautifulSoup(contents, 'html.parser')
 
     links = soup.findAll('a')
-
-    
     for link in links:
-        if link.has_attr('href') and len(link['href']) > len(stem) and link['href'][:len(stem)] == stem:
-            if link['href'] not in all_urls:
-                all_urls.append(link['href'])
-                link_stem_finder(link['href'],stem, depth+1)
+        if link.has_attr('href') and re.search(regex,link['href']):
+            if link['href'] not in all_urls and link['href'] not in bad_urls:
+                try:
+                    link_stem_finder(link['href'],regex, depth+1)
+                except:
+                    bad_urls.append(link['href'])
 
-link_stem_finder(url,stem,depth)
+def custom_reuters():
+    #reters articles don't really link to each other, so the recursive approach doesn't work, lets try this
+    regex = "/article/us"
+    for i in range(1,1000):
+        url = 'https://www.reuters.com/news/archive/domesticnews?page=' + str(i)
+        page = requests.get(url)
+        contents = page.content
 
+        soup = BeautifulSoup(contents, 'html.parser')
+        links = soup.findAll('div',{"class": 'story-content'})
+        temp_links = []
+        for link in links:
+            temp_links += link.findAll('a')
+        links = temp_links
+        for link in links:
+            if link.has_attr('href') and re.search(regex,link['href']):
+                #print("page: " + str(i))
+                #print(link['href'])
+                if link['href'] not in all_urls:
+                    all_urls.append(link['href'])
+                    print(len(all_urls))
+        if len(all_urls) > 1000:
+            return
+
+
+
+custom_reuters()
+#reuters_one_page()
+
+#link_stem_finder(url,regex,depth)
+if True:
+    for url in all_urls:
+        print(url)
+        print()
 print(len(all_urls))
 
-file = open('fox_urls.txt','w')
-for url in all_urls:
-    file.write(url + '\n')
+if False:
+    file = open('website_urls/reuters_urls.txt','w')
+    for url in all_urls:
+        file.write('https://www.reuters.com' + url + '\n')
