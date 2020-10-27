1a0a539b8701a6e40a3663201eb0e574f8bb3bff
Aidan <aidan.bnc@gmail.com>
2020-10-27 14:47:28 -0400

    Changed file structure.

--- webScrapingStuff/article_finder.py
+++ webScrapingStuff/article_finder.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.foxnews.com/politics/faith-and-family-a-look-at-judge-amy-coney-barrett'
+url = 'https://www.foxnews.com/politics/supreme-court-strikes-down-state-ban-on-taxpayer-funding-for-religious-schools'
 
 def getArticle(url,site):
     page = requests.get(url)
@@ -11,28 +11,25 @@ def getArticle(url,site):
     if(site == 'fox'):
         paragraphs = soup.find('div',{"class": 'article-body'}).findAll('p')
         bad_paragraphs = soup.find('div',{"class": 'article-body'}).findAll(href=True)
-        #temp = soup.findAll('div',{"class": 'info'})
-        #bad_paragraphs += temp
+
         #we want to get rid of the annoying links in the middle of the articles
         for i in range(0,len(bad_paragraphs)):
             bad_paragraphs[i] = bad_paragraphs[i].getText()
         returner = ""
         for paragraph in paragraphs:
             temp = paragraph.getText()
-            #print(temp)
-            #print(bad_paragraphs)
             if temp not in bad_paragraphs:
                 returner += temp + " " 
         return returner
-        #soup = BeautifulSoup(article_html,'html.parser')
-        #return soup.findAll('p')
         
     #paragraphs = soup.findAll("div",{"class": html_class})
-    
+    #this is the basic format of returning the paragarphs
+    '''
     returner = ""
     for val in paragraphs:
         returner += val.getText() + " " 
     return returner
+    '''
 
 print(getArticle(url,'fox'))
 #getArticle(url,'fox')
