cb5525ddb6beb983e91d415baa62456ee400e73c
Sharqosity <typingpalguy@gmail.com>
2020-10-27 17:38:57 -0400

    add parsing for reuters

--- webScrapingStuff/article_finder.py
+++ webScrapingStuff/article_finder.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 url = 'https://www.foxnews.com/politics/supreme-court-strikes-down-state-ban-on-taxpayer-funding-for-religious-schools'
 url_nyt = 'https://www.nytimes.com/2020/10/26/us/politics/trump-barrett.html'
 url_usatoday = 'https://www.usatoday.com/story/news/politics/2020/10/27/cincinnati-columbus-lawsuit-seek-overturn-house-bill-6-bailout-tax/3748629001/'
-
+url_reuters = 'https://www.reuters.com/article/us-usa-election-pence/political-adviser-sparked-covid-19-outbreak-on-vice-president-pence-team-sources-idUSKBN27C2X5'
 
 def getArticle(url, site):
     page = requests.get(url)
@@ -42,6 +42,14 @@ def getArticle(url, site):
             if not paragraph.find('strong'):
                 returner += temp + " "
         return returner
+
+    if (site == 'reuters'):
+        paragraphs = soup.find('div', {"class": 'ArticleBodyWrapper'}).findAll('p')
+        returner = ""
+        for paragraph in paragraphs:
+            temp = paragraph.getText()
+            returner += temp + " " 
+        return returner
     # paragraphs = soup.findAll("div",{"class": html_class})
     # this is the basic format of returning the paragarphs
     '''
@@ -53,5 +61,6 @@ def getArticle(url, site):
 
 #print(getArticle(url,'fox'))
 #print(getArticle(url_nyt, 'nyt'))
-print(getArticle(url_usatoday, 'usatoday'))
+#print(getArticle(url_usatoday, 'usatoday'))
+print(getArticle(url_reuters, 'reuters'))
 # getArticle(url,'fox')
