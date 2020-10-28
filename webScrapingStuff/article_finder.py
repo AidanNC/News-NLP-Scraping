fcba71f7bd1c089f2149eb8ab5cee2c3991c5bd7
Sharqosity <typingpalguy@gmail.com>
2020-10-28 18:56:56 -0400

    cnn parsing

--- webScrapingStuff/article_finder.py
+++ webScrapingStuff/article_finder.py
@@ -9,22 +9,33 @@ from bs4 import BeautifulSoup
 #Reuters US news
 #Politico
 #Yahoo News
-#Npr news
-#LA Times
+#--Npr news
+#--LA Times
 #Breitbart
+#Wall street journal
 
-
-url = 'https://www.foxnews.com/politics/supreme-court-strikes-down-state-ban-on-taxpayer-funding-for-religious-schools'
+url_cnn = 'https://www.cnn.com/2020/10/27/politics/justice-amy-coney-barrett-sworn-in-supreme-court/index.html'
+url_fox = 'https://www.foxnews.com/politics/supreme-court-strikes-down-state-ban-on-taxpayer-funding-for-religious-schools'
 url_nyt = 'https://www.nytimes.com/2020/10/26/us/politics/trump-barrett.html'
 url_usatoday = 'https://www.usatoday.com/story/news/politics/2020/10/27/cincinnati-columbus-lawsuit-seek-overturn-house-bill-6-bailout-tax/3748629001/'
 url_reuters = 'https://www.reuters.com/article/us-usa-election-pence/political-adviser-sparked-covid-19-outbreak-on-vice-president-pence-team-sources-idUSKBN27C2X5'
 url_politico = 'https://www.politico.com/news/2020/10/27/no-apologies-mcconnell-says-barrett-a-huge-success-for-the-country-432828'
 url_yahoo = 'https://news.yahoo.com/wisconsin-decision-supreme-court-foreshadows-020446094.html'
 url_yahoo2 = 'https://news.yahoo.com/i-purely-vote-for-my-actual-interests-black-men-could-be-critical-bloc-in-presidential-election-174237805.html'
+
 def getArticle(url, site):
     page = requests.get(url)
     contents = page.content
     soup = BeautifulSoup(contents, 'html.parser')
+    if (site == 'cnn'):
+        #paragraphs = soup.find('div', {"class": 'l-container'}).findAll('div', {"class:" 'zn-body__paragraph'})
+        #Doesn't get the very first paragraph hmm
+        paragraphs = soup.findAll("div",{"class": "zn-body__paragraph"})
+        returner = ""
+        for paragraph in paragraphs:
+            temp = paragraph.getText()
+            returner += temp + " " 
+        return returner
     if(site == 'fox'):
         paragraphs = soup.find('div', {"class": 'article-body'}).findAll('p')
         bad_paragraphs = soup.find('div', {"class": 'article-body'}).findAll(href=True)
@@ -101,10 +112,11 @@ def getArticle(url, site):
     return returner
     '''
 
-#print(getArticle(url,'fox'))
+#print(getArticle(url_fox,'fox'))
 #print(getArticle(url_nyt, 'nyt'))
 #print(getArticle(url_usatoday, 'usatoday'))
 #print(getArticle(url_reuters, 'reuters'))
 #print(getArticle(url_politico, 'politico'))
 #print(getArticle(url_yahoo2, 'yahoo'))
-# getArticle(url,'fox')
+print(getArticle(url_cnn, 'cnn'))
+
